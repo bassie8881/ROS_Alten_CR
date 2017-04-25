8f3b6209a148540510e0319f0e0809ddd80f8c4e
bas <bas_bosscher@hotmail.com>
2017-04-25 10:00:35 +0200

    commit 25-04-2017 --> working model

--- cr_mit/src/marker_viz.cpp
+++ cr_mit/src/marker_viz.cpp
@@ -8,8 +8,6 @@
 geometry_msgs::Point compare;
 geometry_msgs::TransformStamped marker;
 
-int num_points = 0;
-
 void markerTransform(){
     static tf::TransformBroadcaster transformer;
     marker.header.stamp = ros::Time::now();
@@ -34,6 +32,28 @@ int main(int argc, char **argv){
     tf::TransformListener listener;
     ros::Rate rate(10.0);
 
+    visualization_msgs::Marker points, line_strip;
+    points.header.frame_id = line_strip.header.frame_id = "/my_frame";
+    points.header.stamp = line_strip.header.stamp = ros::Time::now();
+    points.ns = line_strip.ns = "points_and_lines";
+    points.action = line_strip.action = visualization_msgs::Marker::ADD;
+    points.pose.orientation.w = line_strip.pose.orientation.w = 1.0;
+
+    points.id = 0;
+    line_strip.id = 1;
+
+    points.type = visualization_msgs::Marker::POINTS;
+    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+
+    points.scale.x = 0.005;
+    points.scale.y = 0.005;
+    line_strip.scale.x = 0.005;
+
+    points.color.g = 1.0f;
+    points.color.a = 1.0;
+    line_strip.color.b = 1.0;
+    line_strip.color.a = 1.0;
+
     while(nh.ok()){
         tf::StampedTransform transform;
         try{
@@ -44,28 +64,6 @@ int main(int argc, char **argv){
             ros::Duration(1.0).sleep();
         }
 
-        visualization_msgs::Marker points, line_strip;
-        points.header.frame_id = line_strip.header.frame_id = "/my_frame";
-        points.header.stamp = line_strip.header.stamp = ros::Time::now();
-        points.ns = line_strip.ns = "points_and_lines";
-        points.action = line_strip.action = visualization_msgs::Marker::ADD;
-        points.pose.orientation.w = line_strip.pose.orientation.w = 1.0;
-
-        points.id = 0;
-        line_strip.id = 1;
-
-        points.type = visualization_msgs::Marker::POINTS;
-        line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-
-        points.scale.x = 0.005;
-        points.scale.y = 0.005;
-        line_strip.scale.x = 0.005;
-
-        points.color.g = 1.0f;
-        points.color.a = 1.0;
-        line_strip.color.b = 1.0;
-        line_strip.color.a = 1.0;
-
         // Message
         geometry_msgs::Point p;
         p.x = transform.getOrigin().x();
@@ -75,15 +73,13 @@ int main(int argc, char **argv){
         markerTransform();
 
         if((p.x != compare.x)||(p.y != compare.y)||(p.z != compare.z)){
+
             points.points.push_back(p);
             line_strip.points.push_back(p);
             compare = p;
-            num_points = num_points +1;
 
-            if(num_points >= 15){
-                marker_pub.publish(points);
-                marker_pub.publish(line_strip);
-            }
+            marker_pub.publish(points);
+            marker_pub.publish(line_strip);
         }
         rate.sleep();
     }
